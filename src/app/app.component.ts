3e10973fe9c41066ed649bd8501015427372cd4f
Yunus Eskisan <yunuseskisan@Yunuss-MacBook-Air.local>
2020-04-28 10:03:26 +0100

    dev working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HelloRequest } from 'project/proto/v1/hello_pb';
+import { HelloServicePromiseClient } from 'project/proto/v1/hello_grpc_web_pb';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'core';
+
+  ngOnInit(): void {
+    const serviceUrl = 'http://localhost:8083';
+    const client: HelloServicePromiseClient = new HelloServicePromiseClient(serviceUrl)
+
+    const request = new HelloRequest()
+    request.setName('123')
+
+    client.sayHello(request).then((err) => {
+      console.error('error requesting graph: %o', err);
+    });
+  }
 }
